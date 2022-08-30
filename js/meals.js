0ed8cb12b0f0057d663c49a5932ba1d7874259c8
Lipton Barua <liptonbarua274@gmail.com>
2022-08-30 13:42:34 +0600

    project

--- js/meals.js
+++ js/meals.js
@@ -4,7 +4,7 @@ const loadmeals = (search)=>{
     .then(res=>res.json())
     .then(data=>displayMeals(data.meals))
 }
-loadmeals('a')
+loadmeals()
 
 const displayMeals =(meals)=>{
 const mealsContainer = document.getElementById('meals-container')
@@ -14,7 +14,7 @@ mealsContainer.innerHTML =''
  const div = document.createElement('div')
  div.classList.add('col')
  div.innerHTML =`
- <div class="card bg-dark text-white">
+ <div onclick="displayIdMeals(${meal.idMeal})" class="card bg-dark text-white">
  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
  <div class="card-body">
    <h5 class="card-title">${meal.strMeal}</h5>
@@ -31,4 +31,28 @@ const btnSearch=()=>{
     const searchValue = searchField.value;
     loadmeals(searchValue)
     searchField.value =''
+}
+
+const displayIdMeals =(idMeal)=>{
+const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+fetch(url)
+.then(res=>res.json())
+.then(data=>displayDetiels(data.meals[0]))
+}
+
+const displayDetiels =(meal)=>{
+ const detielsContainer =document.getElementById('detiels-container');
+ detielsContainer.innerHTML =''
+ const detielsDiv = document.createElement('div')
+ detielsDiv.classList.add('col')
+ detielsDiv.innerHTML = `
+  <div class="card bg-dark text-white" style="width: 22rem;">
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+  <h5 class="card-title">${meal.strMeal}</h5>
+  <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+  </div>
+  </div>
+ `
+ detielsContainer.appendChild(detielsDiv)
 }
